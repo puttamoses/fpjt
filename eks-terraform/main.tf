943b6af7f13ea34031a974769a73e9ecd09bce2d
Moses putta <136316767+puttamoses@users.noreply.github.com>
2024-05-20 16:13:07 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -160,7 +160,7 @@ data "aws_security_group" "selected" {
     ]
   }
  resource "aws_eks_node_group" "node-grp" {
-    cluster_name    = aws_eks_cluster.node-grp.name
+    cluster_name    = aws_eks_cluster.eks.name
     node_group_name = "project-node-group"
     node_role_arn   = aws_iam_role.worker.arn
     subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
@@ -168,7 +168,14 @@ data "aws_security_group" "selected" {
     disk_size       = 20
     instance_types  = ["t2.small"]
 
-    
+    remote_access {
+      ec2_ssh_key               = "provisioner"
+      source_security_group_ids = [data.aws_security_group.selected.id]
+    }
+
+    labels = {
+      env = "dev"
+    }
 
     scaling_config {
       desired_size = 2
