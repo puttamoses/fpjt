60ef40a76e027ddc50d683ff95aff084925e7d71
Moses putta <136316767+puttamoses@users.noreply.github.com>
2024-05-20 17:04:52 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -168,11 +168,7 @@ data "aws_security_group" "selected" {
     disk_size       = 20
     instance_types  = ["t2.small"]
 
-    remote_access {
-      ec2_ssh_key               = "provisioner"
-      source_security_group_ids = [data.aws_security_group.selected.id]
-    }
-
+  
     labels = {
       env = "dev"
     }
