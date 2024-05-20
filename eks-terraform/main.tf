cc72f010da256de20ce10a8b567d6859a6b2370f
Moses putta <136316767+puttamoses@users.noreply.github.com>
2024-05-20 17:33:48 +0530

    Update main.tf

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -164,24 +164,17 @@ data "aws_security_group" "selected" {
     node_group_name = "project-node-group"
     node_role_arn   = aws_iam_role.worker.arn
     subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
-    capacity_type   = "ON_DEMAND"
-    disk_size       = 20
     instance_types  = ["t2.small"]
 
   
-    labels = {
-      env = "dev"
-    }
-
+    
     scaling_config {
       desired_size = 2
       max_size     = 4
       min_size     = 1
     }
 
-    update_config {
-      max_unavailable = 1
-    }
+    
 
     depends_on = [
       aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
